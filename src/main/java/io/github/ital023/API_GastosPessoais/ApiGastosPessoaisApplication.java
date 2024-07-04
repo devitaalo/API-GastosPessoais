b61aae473a8edb4697e55caadaf3354ef7a4363e
Ital02 <devitalomiranda@gmail.com>
2024-07-04 14:09:38 -0300

    feat: first commit

---
+++ src/main/java/io/github/ital023/API_GastosPessoais/ApiGastosPessoaisApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.API_GastosPessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGastosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGastosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ital023/API_GastosPessoais/ApiGastosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.API_GastosPessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGastosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
